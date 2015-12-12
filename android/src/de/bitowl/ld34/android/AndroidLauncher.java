8b0e9c515e83180448c33ff6282b4130a679cce9
bitowl <bitowl@googlemail.com>
2015-12-12 10:18:19 +0100

    project setup

---
+++ android/src/de/bitowl/ld34/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.bitowl.ld34.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.bitowl.ld34.MyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGame(), config);
+	}
+}
---
+++ desktop/src/de/bitowl/ld34/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.bitowl.ld34.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.bitowl.ld34.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
